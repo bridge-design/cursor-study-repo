e4e2eaf16cfa48a7c4820b33c7388e7831b9a140
Varya Stepanova <mail@varya.me>
2025-11-07 22:20:33 +0200

    using header on page

--- src/templates/home.stories.tsx
+++ src/templates/home.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import { Layout } from '@/components';
+import { Header, Layout } from '@/components';
 
 const meta: Meta = {
   title: 'Templates/Home',
@@ -14,7 +14,9 @@ type Story = StoryObj;
 
 const Template = () => (
   <>
+    <Header>Header content</Header>
     <Layout>
+
       Page will be here
     </Layout>
   </>
